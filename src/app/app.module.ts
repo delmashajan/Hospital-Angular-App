ffbd7d4e814cd53b45decc0130c4d49b9a5e05b7
delmashajan <delmashajan@gmail.com>
2022-11-24 13:07:54 +0530

    viewpatientsadded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { PatientEntryComponent } from './patient-entry/patient-entry.component';
 import { PatientSearchComponent } from './patient-search/patient-search.component';
 import { PatientDeleteComponent } from './patient-delete/patient-delete.component';
 import { Route, RouterModule, Routes } from '@angular/router';
+import { ViewPatientComponent } from './view-patient/view-patient.component';
 
 const myRoute:Routes=[
   {
@@ -20,6 +21,11 @@ const myRoute:Routes=[
   {
     path:"delete",
     component:PatientDeleteComponent
+  },
+  {
+    path:"view",
+    component:ViewPatientComponent
+
   }
 
 ]
@@ -29,7 +35,8 @@ const myRoute:Routes=[
     AppComponent,
     PatientEntryComponent,
     PatientSearchComponent,
-    PatientDeleteComponent
+    PatientDeleteComponent,
+    ViewPatientComponent
   ],
   imports: [
     BrowserModule,
